b4b8fa8fdd095a4f4d5513ba22d53f9761444414
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2021-02-12 09:16:23 +0100

    creado package

--- src/MisMetodos.java
+++
@@ -1,6 +0,0 @@
-public class MisMetodos {
-
-    public static String saludo(String mensaje){
-        return "Saldudo "+mensaje;
-    }
-}
---
+++ src/libreriasv8/MisMetodos.java
@@ -0,0 +1,8 @@
+package libreriasv8;
+
+public class MisMetodos {
+
+    public static String saludo(String mensaje) {
+        return "Saldudo " + mensaje;
+    }
+}
